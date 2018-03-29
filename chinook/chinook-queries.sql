95add98ec9e493c4f6d12ee8cc12a4557ab22cea
kylesaldana5 <kylesaldana5@gmail.com>
2018-03-29 11:39:10 -0500

    6 additional querries done

--- chinook/chinook-queries.sql
+++ chinook/chinook-queries.sql
@@ -32,15 +32,57 @@ JOIN Customer c
 ON e.EmployeeID = c.SupportRepID
 JOIN Invoice i 
 ON i.CustomerID = c.CustomerID
-WHERE e.Title = "Sales Agent"
+WHERE e.Title = "Sales Support Agent"
 7. Provide a query that shows the Invoice Total, Customer name, Country and Sale Agent name for all invoices and customers.
+SELECT i.Total, 
+(c.FirstName || " " || c.LastName) FullName,
+(e.FirstName || " " || e.LastName) RepName
+FROM Invoice i
+JOIN customer c
+JOIN Employee e
+WHERE c.SupportRepID = e.EmployeeID
+
 8. How many Invoices were there in 2009 and 2011? What are the respective total sales for each of those years?
+SELECT COUNT(*) AS 'Count', SUM(Total) as 'Total',
+SUBSTR(InvoiceDate, 1,4) as year
+FROM invoice 
+WHERE InvoiceDate LIKE '2009%'
+UNION
+SELECT COUNT(*) AS 'Count', SUM(Total) AS 'Total',
+SUBSTR(InvoiceDate,1,4) AS 'Year'
+FROM Invoice
+WHERE InvoiceDate LIKE '2011%'
+
 9. Looking at the InvoiceLine table, provide a query that COUNTs the number of line items for Invoice ID 37.
+SELECT SUM(Quantity) FROM InvoiceLine
+WHERE InvoiceID = 37
+
 10. Looking at the InvoiceLine table, provide a query that COUNTs the number of line items for each Invoice. HINT: [GROUP BY](http://www.sqlite.org/lang_select.html#resultset)
+SELECT SUM(Quantity), InvoiceID
+FROM InvoiceLine
+GROUP BY InvoiceID
+
 11. Provide a query that includes the track name with each invoice line item.
+SELECT InvoiceLine.*, t.Name
+FROM InvoiceLine, Track t
+WHERE InvoiceLine.InvoiceLineID = t.TrackID
+
 12. Provide a query that includes the purchased track name AND artist name with each invoice line item.
+SELECT InvoiceLine.*, t.Name, artist.Name
+FROM InvoiceLine, Track t, Artist, Album
+WHERE InvoiceLine.InvoiceID = t.TrackID
+AND t.AlbumID = album.AlbumID
+AND album.ArtistID =  artist.ArtistID
+
+
 13. Provide a query that shows the # of invoices per country. HINT: [GROUP BY](http://www.sqlite.org/lang_select.html#resultset)
+SELECT COUNT(*), BillingCountry
+FROM Invoice
+GROUP BY BillingCountry
+
 14. Provide a query that shows the total number of tracks in each playlist. The Playlist name should be included on the resultant table.
+
+
 15. Provide a query that shows all the Tracks, but displays no IDs. The resultant table should include the Album name, Media type and Genre.
 16. Provide a query that shows all Invoices but includes the # of invoice line items.
 17. Provide a query that shows total sales made by each sales agent.
